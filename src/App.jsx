b0dd5caa42dca5efb8088c1b49c05fe6c06e7e00
Chiaraliz <15962100649@163.com>
2024-07-10 19:48:35 +0800

    Initial setup for the app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
